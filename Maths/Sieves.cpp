a4d1dfb3802a38568fadd7e9cc707573f36efaeb
Jorge Luis Ibáñez <39895741+JorgeIba@users.noreply.github.com>
2020-07-27 17:41:05 -0500

    fixing errors in Sieve.cpp

--- Maths/Sieves.cpp
+++ Maths/Sieves.cpp
@@ -21,12 +21,12 @@ void primesSieve(int n)
 
 //12
 vector<int> lowestPrime;
-void lowestPrimeSieve(int n)
-{
+void lowestPrimeSieve(int n){
 	lowestPrime.resize(n + 1, 1);
 	lowestPrime[0] = lowestPrime[1] = 0;
 	for(int i = 2; i <= n; ++i) lowestPrime[i] = (i & 1 ? i : 2);
-	for(lli i = 3; i*i <= limit; i += 2)
+	int limit = sqrt(n);
+	for(int i = 3; i <= limit; i += 2)
 		if(lowestPrime[i] == i)
 			for(int j = i * i; j <= n; j += 2 * i)
 				if(lowestPrime[j] == j) lowestPrime[j] = i;
