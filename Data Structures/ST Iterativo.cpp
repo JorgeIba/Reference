f81d2b73d62bf81ebc1de91437c43dfd6f879af4
Jorge Luis Ibáñez <39895741+JorgeIba@users.noreply.github.com>
2020-09-01 00:30:22 -0500

    Update ST Iterativo.cpp

--- Data Structures/ST Iterativo.cpp
+++ Data Structures/ST Iterativo.cpp
@@ -1,35 +1,71 @@
-//12
-void build() //ST size == 2*n
-{
-    for(int i = n; i<2*n; i++)
-    {
-        ST[i] = nums[i-n];
-    }
-    for(int i = n-1; i>0; i--)
-    {
-        ST[i] = Merge( ST[i<<1] , ST[i<<1|1] ) ;
-    }
-}
-//11
-Node query(lli l, lli r) //Node could be just an integer
-{
-    Node MAX1;
-    Node MAX2;
-    for(l+=n, r+=n; l<=r; l>>=1, r>>=1)
-    {
-        if(l&1) MAX1 = Merge(MAX1, ST[l++]);   
-        if(~r&1) MAX2 = Merge(ST[r--],MAX2);
-    }
-    return Merge(MAX1, MAX2);
-}
-//10
-void update(int index, Node value)
-{
-    index+=n;
-    ST[index] = Merge(value, ST[index]);
-    for(; index; index>>=1)
-    {
-        ST[index>>1] = ST[index] + ST[index^1];
-    }
-}
+template <typename T,typename S> 
+struct SegmentTreeItLazy{ // 0 - indexed
+	T neutro = 0;
+	int size, height;
+	vector<T> st, delUp;
+	vector<bool> upd;
+    
+    //S tipo de dato que recibe
+	SegmentTreeItLazy(int n, T val = 0,  const vector<S> &v = vector<S>()){
+		st.resize(2*n);
+        if(v.empty()) fill(all(st), val);
+		size = n;
+		delUp.resize(n);
+		upd.resize(n);
+		height = sizeof(int) * 8 - __builtin_clz(size);
+        if(!v.empty()) forn(i,n) st[i+size] = set(v[i]);
+		build();
+	}
 
+	T merge(T a, T b){
+		return a + b;
+		//return a * b;
+		//return __gcd(a, b);
+		//return min(a, b);
+	}
+	void apply(int i, T val, int k){
+		st[i] += val * k; 
+		if (i < size) delUp[i] += val, upd[i] = 1;
+	}
+	void calc(int i){
+		if (!upd[i]) st[i] = merge(st[i << 1], st[i << 1 | 1]);
+	}
+	void build(){
+		for(int i = size - 1; i; i--) calc(i);
+	}
+	void build(int p) {
+		while (p > 1) p >>= 1, calc(p);
+	}
+	void push(int p) {
+		for (int s = height, k = 1 << (height - 1); s; s--, k >>= 1){
+			int i = p >> s;
+			if (upd[i]){
+				apply(i << 1, delUp[i], k);
+				apply(i << 1 | 1, delUp[i], k);
+				upd[i] = 0, delUp[i] = 0;
+			}
+		}
+	}
+	void update(int l, int r, T val){
+		push(l += size), push(r += size);
+		int ll = l, rr = r, k = 1;
+		for (; l <= r; l >>= 1, r >>= 1, k <<= 1) {
+			if (l & 1) apply(l++, val, k);
+			if (~r & 1) apply(r--, val, k);
+		}
+		build(ll), build(rr);
+	}
+	T query(int l, int r){
+		push(l += size), push(r += size);
+		T res = neutro;
+		for (; l <= r; l >>= 1, r >>= 1){
+			if (l & 1) res = merge(res, st[l++]);
+			if (~r & 1) res = merge(res, st[r--]);
+		}
+		return res;
+	}
+	T& operator[](int i) { return st[i + size]; }
+	T set(S x){
+		return x;
+	}
+};
