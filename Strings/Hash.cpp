2a425da00a22e73a3bdb17fcdffa1dfc985cece1
Daniel Montes Guerrero <43693529+DanielMontesGuerrero@users.noreply.github.com>
2020-10-10 19:24:29 -0500

    Create Hash.cpp

---
+++ Strings/Hash.cpp
@@ -0,0 +1,21 @@
+struct Hash {
+	lli MOD=212345678987654321LL,P=1777771,PI=106955741089659571LL;
+	vector<lli> h,pi;
+	Hash(string& s){
+		assert((P*PI)%MOD==1);
+		h.resize(s.size()+1);
+		pi.resize(s.size()+1);
+		h[0]=0;
+		pi[0]=1;
+		lli p=1;
+		forr(i,1,s.size()){
+			h[i]=(h[i-1]+p*s[i-1])%MOD;
+			pi[i]=(pi[i-1]*PI)%MOD;
+			p=(p*P)%MOD;
+		}
+	}
+	//returns the hash of substring [s,e)
+	lli get(int s, int e){
+		return (((h[e]-h[s]+MOD)%MOD)*pi[s])%MOD;
+	}
+};
