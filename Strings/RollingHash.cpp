a407ede0cd5338d6dee13299361bbd9bd7d895c4
Jorge Luis Ibáñez <39895741+JorgeIba@users.noreply.github.com>
2020-10-26 19:58:20 -0600

    Upgrade RollingHash.cpp

--- Strings/RollingHash.cpp
+++ Strings/RollingHash.cpp
@@ -1,22 +1,48 @@
 struct RollingHash
 {
     lli hash=0, mod, p, magic, inverse_p, size = 0; 
+    deque<char> curr;
     RollingHash(lli p, lli mod): p(p), mod(mod){ 
         inverse_p = getInverse(); 
         magic = 1;
     }
-    void push(char newAdd)
+    void push_back(char newAdd)
     {
         hash = (hash*p + newAdd*p)%mod;
         magic = (magic*p)%mod;
-        //size++;
+        curr.push_back(newAdd);
     }
-    void pop(char oldErase)
+    char pop_front()
     {
+        char oldErase = curr.front();
         hash = (hash - oldErase*magic)%mod;
         if(hash<0) hash += mod;
         magic = (magic*inverse_p)%mod;
+        return oldErase;
     }
+
+    char pop_back()
+    {
+        char last = curr.back();
+        hash = (hash - curr.back()*p)%mod;
+        if(hash<0) hash += mod;
+        hash = (hash*inverse_p)%mod;
+        magic = (magic*inverse_p)%mod;
+        curr.pop_back();
+        return last;
+    }
+
+    void push_front(char newAdd){
+        magic = (magic*p)%mod;
+        hash = (hash + newAdd*magic)%mod;
+        curr.push_front(newAdd);
+    }
+
+    void shiftRight(){ push_front(pop_back()); }
+
+    void shiftLeft(){ push_back(pop_front()); }
+
+    char getLast(){ return curr.back(); }
     lli getHash(){return hash;}
     lli getInverse()
     {
