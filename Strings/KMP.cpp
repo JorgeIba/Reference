840e0034b1152d2ddc2a592f3ea4c6bfc91d5461
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2022-12-01 12:50:45 -0600

    Add function to count the occurrences of a preffix
    Add a function to count the number of occurrences of a certain preffix
    in a string.
    
    Change the name of the function that calculates preffix function from
    `preffix_array` to `preffix_function`.
    
    Correct bug in `preffix_function` that caused a bad calculation of
    preffix function array.

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -2,14 +2,14 @@
  * preffix array at position i gives us the length of the largest propper
  * preffix which is also a suffix in substring patt[0,...,i]
  */
-vector <int> preffix_array(string& patt){
+vector <int> preffix_function(string& patt){
 	int length = patt.size();
 	vector <int> lps (length, 0);
 	int j = 0;
 	for(int i = 1; i < length; i++) {
 		j = lps[i-1];
 		while(j > 0 && patt[i] != patt[j]) j = lps[j-1];
-		lps[i] = j + patt[i] == patt[j];
+		lps[i] = j + (patt[i] == patt[j]);
 	}
 	return lps;
 }
@@ -18,7 +18,7 @@ vector<int> kmp(string text, string pattern){
 	vector<int> indexes;
 	int i = 0;
 	int j = 0;
-	vector<int> lps = preffix_array(pattern);
+	vector<int> lps = preffix_function(pattern);
 	for(int i = 0; i < text.size(); i++) {
 		while(j > 0 && pattern[j] != text[i]) j = lps[j - 1];
 		if(text[i] == pattern[j]) {
@@ -30,4 +30,21 @@ vector<int> kmp(string text, string pattern){
 		}
 	}
 	return indexes;
+}
+/*
+ * Returns an array of size |s| + 1, the i-th position of this array conains
+ * the number of occurrences of preffix s[1,...,i] (1-based). Position 0
+ * doesn't contain relevant information.
+ */
+vector<int> number_of_occ_preff(string s) {
+	vector<int> pi = preffix_function(s);
+	int len = s.size();
+	vector<int> ans(len + 1);
+	for(int i = 0; i < len; i++)
+		ans[pi[i]]++;
+	for(int i = len - 1; i > 0; i--)
+		ans[pi[i-1]] += ans[i];
+	for(int i = 0; i <= len; i++)
+		ans[i]++;
+	return ans;
 }
