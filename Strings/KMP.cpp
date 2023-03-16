e05dfa72937baaf5f78e3071e55fd120052e4b5c
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2023-03-15 21:15:09 -0600

    Add function to build automaton with preffix function

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -2,34 +2,36 @@
  * preffix array at position i gives us the length of the largest propper
  * preffix which is also a suffix in substring patt[0,...,i]
  */
-vector <int> preffix_function(string& patt){
-	int length = patt.size();
-	vector <int> lps (length, 0);
-	int j = 0;
-	for(int i = 1; i < length; i++) {
-		j = lps[i-1];
-		while(j > 0 && patt[i] != patt[j]) j = lps[j-1];
-		lps[i] = j + (patt[i] == patt[j]);
-	}
-	return lps;
+vector<int> preffix_function(string &patt) {
+  int length = patt.size();
+  vector<int> lps(length, 0);
+  int j = 0;
+  for (int i = 1; i < length; i++) {
+    j = lps[i - 1];
+    while (j > 0 && patt[i] != patt[j])
+      j = lps[j - 1];
+    lps[i] = j + (patt[i] == patt[j]);
+  }
+  return lps;
 }
 // Returns an array of all the indexes of text where pattern can be found
-vector<int> kmp(string text, string pattern){
-	vector<int> indexes;
-	int i = 0;
-	int j = 0;
-	vector<int> lps = preffix_function(pattern);
-	for(int i = 0; i < text.size(); i++) {
-		while(j > 0 && pattern[j] != text[i]) j = lps[j - 1];
-		if(text[i] == pattern[j]) {
-			j++;
-			if(j == pattern.size()) {
-				indexes.push_back(i - j + 1);
-				j = lps[j-1];
-			}
-		}
-	}
-	return indexes;
+vector<int> kmp(string text, string pattern) {
+  vector<int> indexes;
+  int i = 0;
+  int j = 0;
+  vector<int> lps = preffix_function(pattern);
+  for (int i = 0; i < text.size(); i++) {
+    while (j > 0 && pattern[j] != text[i])
+      j = lps[j - 1];
+    if (text[i] == pattern[j]) {
+      j++;
+      if (j == pattern.size()) {
+        indexes.push_back(i - j + 1);
+        j = lps[j - 1];
+      }
+    }
+  }
+  return indexes;
 }
 /*
  * Returns an array of size |s| + 1, the i-th position of this array conains
@@ -37,16 +39,16 @@ vector<int> kmp(string text, string pattern){
  * Position 0 doesn't contain relevant information.
  */
 vector<int> number_of_occ_preff(string s) {
-	vector<int> pi = preffix_function(s);
-	int len = s.size();
-	vector<int> ans(len + 1);
-	for(int i = 0; i < len; i++)
-		ans[pi[i]]++;
-	for(int i = len - 1; i > 0; i--)
-		ans[pi[i-1]] += ans[i];
-	for(int i = 0; i <= len; i++)
-		ans[i]++;
-	return ans;
+  vector<int> pi = preffix_function(s);
+  int len = s.size();
+  vector<int> ans(len + 1);
+  for (int i = 0; i < len; i++)
+    ans[pi[i]]++;
+  for (int i = len - 1; i > 0; i--)
+    ans[pi[i - 1]] += ans[i];
+  for (int i = 0; i <= len; i++)
+    ans[i]++;
+  return ans;
 }
 /*
  * This function returns the minimum length of a string that is the
@@ -54,9 +56,25 @@ vector<int> number_of_occ_preff(string s) {
  * function returns 3
  */
 int length_of_min_compression(string s) {
-	vector<int> pi = preffix_function(s);
-	int n = s.size();
-	int k = n - pi[n-1];
-	if(n % k == 0) return k;
-	return n;
+  vector<int> pi = preffix_function(s);
+  int n = s.size();
+  int k = n - pi[n - 1];
+  if (n % k == 0)
+    return k;
+  return n;
+}
+
+void compute_automaton(string s, vector<vector<int>> &aut) {
+  s += '#';
+  int n = s.size();
+  vector<int> pi = prefix_function(s);
+  aut.assign(n, vector<int>(26));
+  for (int i = 0; i < n; i++) {
+    for (int c = 0; c < 26; c++) {
+      if (i > 0 && 'a' + c != s[i])
+        aut[i][c] = aut[pi[i - 1]][c];
+      else
+        aut[i][c] = i + ('a' + c == s[i]);
+    }
+  }
 }
