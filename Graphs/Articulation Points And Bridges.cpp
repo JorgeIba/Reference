6775ede09f9ad7ba713a12f2b257ced980e96b16
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2024-02-20 23:05:28 -0600

    Add biconnected components

--- Graphs/Articulation Points And Bridges.cpp
+++ Graphs/Articulation Points And Bridges.cpp
@@ -12,6 +12,10 @@ vector<vector<int>> adj;
 vector<int> disc, low, isAp;
 vector<Edge> br;
 
+// to get biconnected components
+vector<vector<int>> bcc;
+stack<int> S;
+
 void init(int N) { adj.assign(N, vector<int>()); }
 // 4
 void addEdge(int u, int v) {
@@ -22,13 +26,28 @@ void addEdge(int u, int v) {
 int dfsAPB(int u, int p) {
     int ch = 0, nup = 0;
     low[u] = disc[u] = ++Time;
+
+    // to get biconnected components
+    S.push(u);
+
     for (int &v : adj[u]) {
         if (v == p && !nup++)
             continue;
         if (!disc[v]) {
             ch++, dfsAPB(v, u);
-            if (disc[u] <= low[v])
+            if (disc[u] <= low[v]){
                 isAp[u]++;
+
+                // to get biconnected components
+                bcc.push_back({});
+                bcc.back().push_back(u);
+                while(true){
+                    int x = S.top();
+                    S.pop();
+                    bcc.back().pb(x);
+                    if(x == v) break;
+                }
+            }
             if (disc[u] < low[v])
                 br.push_back({u, v});
             low[u] = min(low[u], low[v]);
@@ -40,7 +59,9 @@ int dfsAPB(int u, int p) {
 // 8
 // O(N)
 void APB() {
+    bcc.clear();
     br.clear();
+    while(!S.empty()) S.pop();
     isAp = low = disc = vector<int>(adj.size());
     Time = 0;
     for (int u = 0; u < adj.size(); u++)
