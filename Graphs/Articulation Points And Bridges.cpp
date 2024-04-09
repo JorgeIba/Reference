ca0e0f2d5b6791c86a328f569ab61811cccca244
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2024-04-08 22:41:10 -0600

    Erase unnecesary new lines in Articulation Points

--- Graphs/Articulation Points And Bridges.cpp
+++ Graphs/Articulation Points And Bridges.cpp
@@ -1,52 +1,44 @@
-// 6
-// APB = articulation points and bridges
-// Ap = Articulation Point
-// br = bridges, p = parent
-// disc = discovery time
-// low = lowTime, ch = children
-// nup = number of edges from u to p
-// 5
 typedef pair<int, int> Edge;
 int Time;
 vector<vector<int>> adj;
 vector<int> disc, low, isAp;
 vector<Edge> br;
-
 // to get biconnected components
 vector<vector<int>> bcc;
 stack<int> S;
 
 void init(int N) { adj.assign(N, vector<int>()); }
-// 4
 void addEdge(int u, int v) {
     adj[u].push_back(v);
     adj[v].push_back(u);
 }
-// 15
 int dfsAPB(int u, int p) {
     int ch = 0, nup = 0;
     low[u] = disc[u] = ++Time;
 
-    // to get biconnected components
+    // -> to get biconnected components
     S.push(u);
+    // <-
 
     for (int &v : adj[u]) {
         if (v == p && !nup++)
             continue;
         if (!disc[v]) {
             ch++, dfsAPB(v, u);
-            if (disc[u] <= low[v]){
+            if (disc[u] <= low[v]) {
                 isAp[u]++;
 
-                // to get biconnected components
+                // -> to get biconnected components
                 bcc.push_back({});
                 bcc.back().push_back(u);
-                while(true){
+                while (true) {
                     int x = S.top();
                     S.pop();
                     bcc.back().pb(x);
-                    if(x == v) break;
+                    if (x == v)
+                        break;
                 }
+                // <-
             }
             if (disc[u] < low[v])
                 br.push_back({u, v});
@@ -56,12 +48,11 @@ int dfsAPB(int u, int p) {
     }
     return ch;
 }
-// 8
-// O(N)
 void APB() {
     bcc.clear();
     br.clear();
-    while(!S.empty()) S.pop();
+    while (!S.empty())
+        S.pop();
     isAp = low = disc = vector<int>(adj.size());
     Time = 0;
     for (int u = 0; u < adj.size(); u++)
